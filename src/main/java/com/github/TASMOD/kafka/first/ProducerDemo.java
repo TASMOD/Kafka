da01094082a4a5f5fdff56e7b376d494c8515afe
TASMOD <t.s.modrakowski@students.uu.nl>
2020-10-01 15:17:21 +0200

    Finishing up the Producer

--- src/main/java/com/github/TASMOD/kafka/first/ProducerDemo.java
+++ src/main/java/com/github/TASMOD/kafka/first/ProducerDemo.java
@@ -18,22 +18,32 @@ public class ProducerDemo {
         Properties properties = new Properties();
 
         /* Old way
-        //Properties can be found at kafka documentation (https://kafka.apache.org/documentation/#producerconfigs)
+        //Properties can be found at kafka documentation
+        (https://kafka.apache.org/documentation/#producerconfigs)
         properties.setProperty("bootstrap.servers", bootstrapServers);
         // These will help the producer understand the data types we are sending to kafka
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
         */
 
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
 
         // 2. Create the producer
         // Producer extends <K, V> both of value string.
-        final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
-        // create a producer record. Topic is auto generated, not hard coded (it appears magically dont be afraid!)
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world");
+        final KafkaProducer<String, String> producer = new KafkaProducer<String,
+                String>(properties);
+        // create a producer record. Topic is auto generated
+        // it's not hard coded (it appears magically don't be afraid!)
+        ProducerRecord<String, String> record = new ProducerRecord<String,
+                String>("first_topic",
+                "key_1",
+                "hello world");
+
         // 3. Send data (asynchronous)
         producer.send(record, new Callback() {
             public void onCompletion(RecordMetadata recordMetadata, Exception e) {
